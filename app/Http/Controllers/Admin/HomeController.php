be64d886d00ba635cf4dbbda3fe7c53d545252c8
ThubeesanKugathas <Thubeesan@outlook.com>
2022-03-11 18:39:08 +0100

    admin folders

---
+++ app/Http/Controllers/Admin/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function __construct() 
+    {
+        $this->middleware('auth');
+    }
+    
+    public function index() {
+        $user = Auth::user();
+    
+        return view('admin.home');
+    }
+}
+
