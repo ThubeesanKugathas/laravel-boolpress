48453f85e2022012a11a0776a402e83b017bb799
ThubeesanKugathas <Thubeesan@outlook.com>
2022-03-14 08:32:14 +0100

    adding vue component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,18 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <div id="appVue">
+        
+    </div>
 
-@section('content')
-<h1>HOME</h1>
-@endsection
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/vue.js') }}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
@@ -28,3 +28,7 @@ Route::middleware('auth')
     });
 
 //  todo after rivisto tutta la lezione
+
+Route::get("{any?}", function() {
+    return view("home");
+})->where("any", ".*");
